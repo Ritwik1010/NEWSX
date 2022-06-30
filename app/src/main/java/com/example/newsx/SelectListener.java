98512691387147407a1e37abe39d1b2944ab56ef
HARSHAL RITWIK <91392069+Ritwik1010@users.noreply.github.com>
2022-06-30 17:09:15 +0530

    NewsX

---
+++ app/src/main/java/com/example/newsx/SelectListener.java
@@ -0,0 +1,9 @@
+package com.example.newsx;
+
+import com.example.newsx.Models.NewsHeadlines;
+
+public interface SelectListener {
+    //void onNewsClicked(NewsHeadlines headlines);
+
+    void OnNewsClicked(NewsHeadlines headlines);
+}
